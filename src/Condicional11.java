20e2aacf46617759ecd849f210d7322f7a733f82
jmarara529 <jmarara529@g.educaand.es>
2023-10-11 10:43:17 +0200

    terminado Condicional11

--- src/Condicional11.java
+++ src/Condicional11.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Condicional11 {
+
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("introduce un numero: ");
+        int num = entrada.nextInt();
+
+        entrada.close();
+
+        double par = num % 2;
+
+        if (par == 0) {
+            System.out.println("el numero " + num + " es par.");
+        } else {
+            System.out.println("el numero " + num + " no es par");
+        }
     }
 }
+
